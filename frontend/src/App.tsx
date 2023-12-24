b755af18da21ad5b696ee1bdfe42fc3a9376d18a
AbdallahAyb <42030466@students.liu.edu.lb>
2023-12-24 19:13:28 +0200

    added some frontend stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+    return (
+        <form action = "http://localhost/jaafar-nigger/webadvanced/backend/login.php" method ="post">
+        username : <input type = "text" name = "user" /> <br></br>
+        Password : <input type = "password" name = "pass" /> <br></br>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button type = "submit" >Submit </button>
+        <button type = "reset" >Cancel </button>
+</form>
+      );
 }
-
-export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,css,scss}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,js,jsx,ts,tsx,css,scss}'],
+  },
+};
